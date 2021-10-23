ee4cea424a86c6b6571793e89c1f56a1917da9cc
edtorrente <edtorrente@sbcglobal.net>
2021-10-23 14:02:01 -0700

    final

---
+++ Instructions/README.md
@@ -0,0 +1,2 @@
+# python-api-challenge
+Apply Python requests, APIs, and JSON to Python API homework
---
+++ Instructions/starter_code/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "1cc8b2cba1c9c82d5b40cbaebf4b290c"
+
+# Google API Key
+g_key = "AIzaSyCABYDll67Bhf1UThtwINIxlPo6VaIRZDw"
+
+
